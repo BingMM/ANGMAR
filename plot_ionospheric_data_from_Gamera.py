5260661bdf315e147ae748fb4050d2b0f5202947
bingMM <michael.madelaire@uib.no>
2025-07-16 16:39:04 +0200

    stuff

--- plot_ionospheric_data_from_Gamera.py
+++ plot_ionospheric_data_from_Gamera.py
@@ -1,29 +1,47 @@
 #%% Import
 
 import os
+import numpy as np
 import matplotlib.pyplot as plt
-from pickle import load
 from polplot import Polarplot
+import h5py
+from datetime import datetime
+from dipole import Dipole
+from tqdm import tqdm
 
 #%% Paths
 
 cwd = os.getcwd()
 #base = os.path.abspath(os.path.join(cwd, '..'))
 base = cwd
-path_in = os.path.join(base, 'data', 'ionospheric_data_from_Gamera.pkl')
+path_in = os.path.join(base, 'data', 'ionospheric_data_from_Gamera.h5')
 path_out = os.path.join(base, 'figures')
 
 #%% Import data
 
-with open(path_in, 'rb') as file:
-    dat = load(file)
-
-'''
-print(dat[0].keys())
+keys = ['lat', 'lon', 'SH', 'SP', 'FAC', 'JHe', 'JHn', 'JPe', 'JPn', 'Be', 'Bn', 'Bu']
+dat = []
+with h5py.File(path_in, 'r') as hf:
+    for step in hf.values():
+        record = {key: step[key][()] for key in keys}
+        record['time'] = datetime.fromisoformat(step.attrs['time'])
+        dat.append(record)
 
 #%%
 
-fig = plt.figure(figsize=(10,10))
-ax = plt.gca()
-pax = Polarplot(ax)
-'''
+for i in tqdm(range(len(dat)), total=len(dat)):
+    dpl = Dipole(epoch=dat[i]['time'].year)
+    mlat, mlon = dpl.geo2mag(dat[i]['lat'], dat[i]['lon'])
+    mlt = dpl.mlon2mlt(mlon, dat[i]['time'])
+    var = dat[i]['FAC']
+    vmax = np.max(abs(var))
+    clvls = np.linspace(-vmax, vmax, 40)
+
+    fig = plt.figure(figsize=(10,10))
+    ax = plt.gca()
+    pax = Polarplot(ax)
+    pax.contourf(mlat, mlt, dat[i]['FAC'], cmap='bwr', levels=clvls)
+    plt.title(dat[i]['time'])
+
+
+
