aa347034f68ee39f91120aacfb5d54522004211b
bingMM <michael.madelaire@uib.no>
2025-07-21 15:05:57 +0200

    Several things

--- plot_ionospheric_data_from_Lompe.py
+++ plot_ionospheric_data_from_Lompe.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from polplot import Polarplot
 import h5py
-from datetime import datetime
+from datetime import datetime, timedelta
 from dipole import Dipole
 from tqdm import tqdm
 from apexpy import Apex
@@ -29,11 +29,6 @@ with h5py.File(path_in, 'r') as hf:
         record['time'] = datetime.fromisoformat(step.attrs['time'])
         dat.append(record)
 
-for key in dat[0].keys():
-    print(key)
-    print(dat[0][key].shape)
-
-
 #%% Define grid
 
 print('Defining CS grid')
@@ -41,25 +36,25 @@ print('Defining CS grid')
 position = (270, 79) # lon, lat for center of the grid
 orientation = 0.
 L = 45e6
-Lres = 60e3
+Lres = 200e3#60e3
 grid = lompe.cs.CSgrid(lompe.cs.CSprojection(position, orientation), L, L, Lres, Lres, R = 6380e3 + 120e3)
 
 #%% Convert to magnetic
 
-for i in tqdm(range(len(dat)), total=len(dat)):
+for i in tqdm(range(len(dat)), total=len(dat), desc='Converting to magnetic'):
     time = dat[i]['time']    
     dpl = Dipole(epoch=time.year)
     
-    _, _, Je, Jn = dpl.geo2mag(dat[i]['lat'], dat[i]['lon'], dat[i]['Je'], dat[i]['Jn'])
+    _, _, Je, Jn = dpl.geo2mag(grid.lat, grid.lon, dat[i]['Je'], dat[i]['Jn'])
     dat[i]['Je'], dat[i]['Jn'] = Je, Jn
     
-    _, _, Be, Bn = dpl.geo2mag(dat[i]['lat'], dat[i]['lon'], dat[i]['Be'], dat[i]['Bn'])
+    _, _, Be, Bn = dpl.geo2mag(grid.lat_mesh, grid.lon_mesh, dat[i]['Be'], dat[i]['Bn'])
     dat[i]['Be'], dat[i]['Bn'] = Be, Bn
 
-#%%
+#%% Plot data
 
-for key in tqdm(['SH', 'SP', 'FAC', 'JHe', 'JHn', 'JPe', 'JPn', 'Be', 'Bn', 'Bu'], total=10):
-    folder = os.path.join(path_out, 'plot_Gamera', key)
+for key in tqdm(['SH', 'SP', 'FAC', 'FAC_int', 'Bu'], total=10):
+    folder = os.path.join(path_out, 'plot_Lompe', key)
     os.makedirs(folder, exist_ok=True)
     for i in range(len(dat)):
         fn = os.path.join(folder, f'{i}.png')
@@ -69,9 +64,15 @@ for key in tqdm(['SH', 'SP', 'FAC', 'JHe', 'JHn', 'JPe', 'JPn', 'Be', 'Bn', 'Bu'
         dpl = Dipole(epoch=time.year)
         apx = Apex(time)
         
-        mlat, mlon = dpl.geo2mag(dat[i]['lat'], dat[i]['lon'])
-        mlt = dpl.mlon2mlt(mlon, dat[i]['time'])
         var = dat[i][key]
+        if var.shape[0] == grid.shape[0]:
+            lat, lon = grid.lat, grid.lon
+        else:
+            lat, lon = grid.lat_mesh, grid.lon_mesh
+        
+        mlat, mlon = dpl.geo2mag(lat, lon)
+        mlt = dpl.mlon2mlt(mlon, time)
+        
         if key == 'SH' or key == 'SP':
             vmax = np.max(var)
             clvls = np.linspace(0, vmax, 40)
@@ -80,17 +81,105 @@ for key in tqdm(['SH', 'SP', 'FAC', 'JHe', 'JHn', 'JPe', 'JPn', 'Be', 'Bn', 'Bu'
             vmax = np.max(abs(var))
             clvls = np.linspace(-vmax, vmax, 40)
             cmap = 'bwr'
-    
+            
         plt.ioff()
         fig = plt.figure(figsize=(10,10))
         ax = plt.gca()
         pax = Polarplot(ax)
         pax.coastlines(time, mag=apx)
-        pax.contourf(mlat, mlt, var, cmap=cmap, levels=clvls)
-        plt.title(dat[i]['time'])
+        pax.tricontourf(mlat.flatten(), mlt.flatten(), var.flatten(), cmap=cmap, levels=clvls)
+        
+        if key == 'FAC' or key == 'Bu':
+            if key == 'FAC':
+                ve, vn = dat[i]['Je'], dat[i]['Jn']
+            else:
+                ve, vn = dat[i]['Be'], dat[i]['Bn']
+            qmax = np.max(np.sqrt(ve**2 + vn**2))*10
+            s=1
+            pax.quiver(mlat[::s, ::s].flatten(), mlt[::s, ::s], vn[::s, ::s].flatten(), ve[::s, ::s].flatten(), scale=qmax, width=.001)
+                
+        plt.title(f"{dat[i]['time']} : {key}")
         plt.savefig(fn, bbox_inches='tight')
         plt.close('all')
         plt.ion()
+
+#%% Rotate and test positions
+
+folder = os.path.join(path_out, 'off_set_tests')
+os.makedirs(folder, exist_ok=True)
+
+
+for i in tqdm(range(len(dat)), total=len(dat), desc='Rotating to test off-set'):
+    fn = os.path.join(folder, f'{i}.png')
+    
+    time = dat[i]['time']
+        
+    dpl = Dipole(epoch=time.year)    
+        
+    var = dat[i]['FAC']
+    lat, lon = grid.lat, grid.lon
+    
+    mlat, mlon = dpl.geo2mag(lat, lon)
+    mlt = dpl.mlon2mlt(mlon, time)
+    
+    vmax = np.max(abs(var))
+    clvls = np.linspace(-vmax, vmax, 40)
+    cmap = 'bwr'
+    
+    plt.ioff()
+    fig, axs = plt.subplots(4, 6, figsize=(30, 20))
+    paxs = [Polarplot(ax) for ax in axs.flatten()]
+    for offset, (pax, ax) in enumerate(zip(paxs, axs.flatten())):
+        pax.tricontourf(mlat.flatten(), mlt.flatten(), var.flatten(), cmap=cmap, levels=clvls)
+        ax.set_title(offset)
+        
+        time_ = time + timedelta(hours=offset)
+        pax.coastlines(time_, mag=Apex(time_))
+                
+    plt.suptitle(f"{time}")
+    plt.savefig(fn, bbox_inches='tight')
+    plt.close('all')
+    plt.ion()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ plot_stations.py
@@ -0,0 +1,77 @@
+#%%
+
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from polplot import Polarplot
+import lompe
+from apexpy import Apex
+
+#%% Paths
+
+cwd = os.getcwd()
+#base = os.path.abspath(os.path.join(cwd, '..'))
+base = cwd
+path_in = os.path.join(base, 'data', '20250717-14-57-supermag-stations.csv')
+path_out = os.path.join(base, 'figures')
+
+#%% Import gmag location
+
+st_data = pd.read_csv(path_in, usecols=range(6))
+
+lat = st_data['GEOLAT'].to_numpy()
+lon = st_data['GEOLON'].to_numpy()[lat>0]
+lat = lat[lat>0]
+lt = lon / 15
+
+#%% Define grids
+
+print('Defining CS grid')
+
+position = (25, 68) # lon, lat for center of the grid
+orientation = 8
+L = 1.1e6
+Lres = 10e3#60e3
+grid_s = lompe.cs.CSgrid(lompe.cs.CSprojection(position, orientation), L, L, Lres, Lres, R = 6380e3 + 120e3)
+
+position = (210, 66) # lon, lat for center of the grid
+orientation = -12
+L = 1.4e6
+Lres = 10e3#60e3
+grid_a = lompe.cs.CSgrid(lompe.cs.CSprojection(position, orientation), L, L, Lres, Lres, R = 6380e3 + 120e3)
+
+position = (255, 62) # lon, lat for center of the grid
+orientation = -12
+L = 3e6
+W = 2.4e6
+Lres = 10e3#60e3
+grid_c = lompe.cs.CSgrid(lompe.cs.CSprojection(position, orientation), L, W, Lres, Lres, R = 6380e3 + 120e3)
+
+#%% Plot all stations
+
+plt.ioff()
+fig = plt.figure(figsize=(15, 15))
+ax = plt.gca()
+pax = Polarplot(ax)
+pax.coastlines(color='k', linewidth=2, zorder=1)
+pax.coastlines(color='cyan', zorder=2)
+pax.scatter(lat, lt, color='tab:orange', zorder=3)
+
+apex = Apex(date=2013)
+for la in np.arange(30, 90, 10):
+    la, lo, _ = apex.apex2geo(np.ones(1000)*la, np.linspace(0, 360, 1000), height=0)
+    pax.plot(la, (lo/15)%24, color='tab:red', zorder=0)
+for lo in np.arange(0, 360, 30):
+    la, lo, _ = apex.apex2geo(np.linspace(0, 90, 1000), np.ones(1000)*lo, height=0)
+    pax.plot(la, (lo/15)%24, color='tab:red', zorder=0)
+
+for grid in [grid_s, grid_a, grid_c]:
+    pax.plot(grid.lat_mesh[0,  :], (grid.lon_mesh[0,  :]/15)%360, color='k', linewidth=3)
+    pax.plot(grid.lat_mesh[-1, :], (grid.lon_mesh[-1, :]/15)%360, color='k', linewidth=3)
+    pax.plot(grid.lat_mesh[:,  0], (grid.lon_mesh[:,  0]/15)%360, color='k', linewidth=3)
+    pax.plot(grid.lat_mesh[:, -1], (grid.lon_mesh[:, -1]/15)%360, color='k', linewidth=3)
+
+plt.savefig(os.path.join(path_out, 'all_stations.png'))
+plt.close('all')
+plt.ion()
